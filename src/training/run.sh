71be4fc64f5275b6cd35b84419dd71b8ca9f2602
Ubuntu <ubuntu@ip-10-0-0-14.ec2.internal>
2018-09-30 18:32:30 +0000

    Updated train_model.py and run.sh to include databreak sparkdl packagewq

--- src/training/run.sh
+++ src/training/run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-export BUCKET=insight-deep-images-hub/MNIST
+
 export MASTER=spark://$(hostname):7077
 export SPARK_WORKER_INSTANCES=3
+export PYSPARK_PYTHON=/home/ubuntu/shrink_venv/bin/python3
+export PYSPARK_DRIVER_PYTHON=/home/ubuntu/shrink_venv/bin/python3
 export CORES_PER_WORKER=1
 export TOTAL_CORES=$((${CORES_PER_WORKER}*${SPARK_WORKER_INSTANCES}))
 export AWS_REGION=us-east-1
 
 ${SPARK_HOME}/bin/spark-submit \
 --master ${MASTER} \
+--packages databricks:spark-deep-learning:1.2.0-spark2.3-s_2.11 \
 --conf spark.cores.max=${TOTAL_CORES} \
 --conf spark.task.cpus=${CORES_PER_WORKER} \
 --conf spark.task.maxFailures=1 \
@@ -16,4 +19,3 @@ ${SPARK_HOME}/bin/spark-submit \
 --conf spark.executorEnv.JAVA_HOME="$JAVA_HOME" \
 --conf spark.executorEnv.AWS_REGION=${AWS_REGION} \
 train_model.py
-
--- src/training/train_model.py
+++ src/training/train_model.py
@@ -31,17 +31,50 @@ from __future__ import print_function
 
 from pyspark.ml.image import ImageSchema
 from pyspark.sql.functions import lit
-from sparkdl.image import imageIO
+from sparkdl.image import imageIO as imageIO
 
 
-banana_image_df = ImageSchema.readImages("hdfs://ec2-18-235-62-224.compute-1.amazonaws.com:9000/OID/Dataset/test/Banana").withColumn("label", lit("banana"))
+#
+# tulips_df = imageIO.readImagesWithCustomFn("hdfs://ec2-18-235-62-224.compute-1.amazonaws.com:9000/" + "flower_photos/tulips", decode_f=imageIO.PIL_decode).withColumn("label", lit(0))
+# daisy_df = imageIO.readImagesWithCustomFn("hdfs://ec2-18-235-62-224.compute-1.amazonaws.com:9000/" + "flower_photos/daisy", decode_f=imageIO.PIL_decode).withColumn("label", lit(0))
+# tulips_train, tulips_test, _ = tulips_df.randomSplit([0.005, 0.005, 0.99])  # use larger training sets (e.g. [0.6, 0.4] for non-community edition clusters)
+# daisy_train, daisy_test, _ = daisy_df.randomSplit([0.005, 0.005, 0.99])     # use larger training sets (e.g. [0.6, 0.4] for non-community edition clusters)
+# train_df = tulips_train.unionAll(daisy_train)
+# test_df = tulips_test.unionAll(daisy_test)
+#
+# # Under the hood, each of the partitions is fully loaded in memory, which may be expensive.
+# # This ensure that each of the paritions has a small size.
+# train_df = train_df.repartition(100)
+# test_df = test_df.repartition(100)
+#
+#
+# from pyspark.ml.classification import LogisticRegression
+# from pyspark.ml import Pipeline
+# from sparkdl import DeepImageFeaturizer
+#
+# featurizer = DeepImageFeaturizer(inputCol="image", outputCol="features", modelName="InceptionV3")
+# lr = LogisticRegression(maxIter=20, regParam=0.05, elasticNetParam=0.3, labelCol="label")
+# p = Pipeline(stages=[featurizer, lr])
+#
+# p_model = p.fit(train_df)
+#
+#
+# from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+#
+# tested_df = p_model.transform(test_df)
+# evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
+# print("Test set accuracy = " + str(evaluator.evaluate(tested_df.select("prediction", "label"))))
 
 
-banana_image_df = banana_image_df.withColumn("prefix", lit('Entity/data/food/fruit/'))
 
-accordion_image_df = ImageSchema.readImages("hdfs://ec2-18-235-62-224.compute-1.amazonaws.com:9000/OID/Dataset/test/Accordion").withColumn("label", lit("accordion"))
+banana_image_df = ImageSchema.readImages("hdfs://ec2-18-235-62-224.compute-1.amazonaws.com:9000/OID/Dataset/test/Banana").withColumn("label", lit(1))
 
-accordion_image_df = accordion_image_df.withColumn("prefix", lit('Entity/data/food/fruit/'))
+
+# banana_image_df = banana_image_df.withColumn("prefix", lit('Entity/data/food/fruit/'))
+
+accordion_image_df = ImageSchema.readImages("hdfs://ec2-18-235-62-224.compute-1.amazonaws.com:9000/OID/Dataset/test/Accordion").withColumn("label", lit(0))
+
+# accordion_image_df = accordion_image_df.withColumn("prefix", lit('Entity/data/food/fruit/'))
 
 
 banana_train, banana_test, _ = banana_image_df.randomSplit([0.99, 0.005, 0.005])  # use larger training sets (e.g. [0.6, 0.4] for non-community edition clusters)
@@ -69,10 +102,10 @@ featurizer = DeepImageFeaturizer(inputCol="image", outputCol="features", modelNa
 lr = LogisticRegression(maxIter=20, regParam=0.05, elasticNetParam=0.3, labelCol="label")
 p = Pipeline(stages=[featurizer, lr])
 
-model = p.fit(train_images_df)    # train_images_df is a dataset of images and labels
+p_model = p.fit(train_df)    # train_images_df is a dataset of images and labels
 
 # Inspect training error
-df = model.transform(train_images_df.limit(10)).select("image", "probability",  "uri", "label")
-predictionAndLabels = df.select("prediction", "label")
+tested_df = p_model.transform(test_df.limit(10))
+predictionAndLabels = tested_df.select("prediction", "label")
 evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
 print("Training set accuracy = " + str(evaluator.evaluate(predictionAndLabels)))
