d9f892e5c32a0c52042f42fc7392cb9cb52f1f3f
EC2 Default User <ec2-user@ip-10-0-0-7.ec2.internal>
2018-10-09 02:03:15 +0000

    added executions/batch_image_submissions.sh

--- executions/batch_image_submissions.sh
+++ executions/batch_image_submissions.sh
@@ -1,38 +1,5 @@
-#!/usr/bin/env python2
-# requester.py
-# ---------------
-# Author: Zhongheng Li
-# Init Date: 10-01-2018
-# Updated Date: 10-08-2018
+#!/bin/bash
 
-"""
-
-
-
-"""
-
-
-from __future__ import print_function
-from airflow.operators.bash_operator import BashOperator
-from airflow.models import DAG
-from datetime import datetime
-
-args = {
-    'owner': 'airflow',
-    'start_date': datetime.now(),
-}
-
-dag = DAG(
-    dag_id='Batch_image_submission_simulation', default_args=args,
-    schedule_interval=None)
-
-
-def print_context(i):
-    print(i)
-    return 'print_context has sucess {}'.format(i)
-
-
-templated_command_1 = """
 
 
 split -l 160 ~/all_labels.txt ~/sample_labels_validation_
@@ -45,20 +12,6 @@ peg scp to-rem pySpark-cluster 3 ~/sample_labels_validation_ac /home/ubuntu/samp
 
 peg scp to-rem pySpark-cluster 4 ~/sample_labels_validation_ad /home/ubuntu/sample_labels_validation_ad
 
-"""
-
-
-Labels_files_prep = \
-    BashOperator(
-        task_id='Data_file_prep',
-        bash_command=templated_command_1,
-        dag=dag)
-
-
-
-
-
-templated_command_2 = """
 
 peg sshcmd-node pySpark-cluster 1 "nohup sh ~/Deep_Images_Hub/src/producer/auto_upload_for_batch.sh ~/sample_labels_validation_aa "validation"  > ~/Deep_Images_Hub/src/producer/auto_upload.log &" 
 
@@ -67,23 +20,6 @@ peg sshcmd-node pySpark-cluster 2 "nohup sh ~/Deep_Images_Hub/src/producer/auto_
 peg sshcmd-node pySpark-cluster 3 "nohup sh ~/Deep_Images_Hub/src/producer/auto_upload_for_batch.sh ~/sample_labels_validation_ac "validation"  > ~/Deep_Images_Hub/src/producer/auto_upload.log &" 
 
 peg sshcmd-node pySpark-cluster 4 "nohup sh ~/Deep_Images_Hub/src/producer/auto_upload_for_batch.sh ~/sample_labels_validation_ad "validation"  > ~/Deep_Images_Hub/src/producer/auto_upload.log &" 
-
-
-
-"""
-
-
-
-
-Batch_image_submissions = \
-    BashOperator(
-        task_id='batch_image_submissions',
-        bash_command=templated_command_2,
-        dag=dag)
-
-
-
-Labels_files_prep >> Batch_image_submissions
 
 
 
