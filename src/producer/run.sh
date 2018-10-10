b0dcd601eed9f4a1198f07af0e6bfa43b6cde011
Heng2j <heng2j@gmail.com>
2018-10-09 21:23:06 -0400

    Added src/producer/producer_distributed.py

--- src/producer/run.sh
+++ src/producer/run.sh
@@ -5,18 +5,23 @@ export MASTER=spark://$(hostname):7077
 export SPARK_WORKER_INSTANCES=3
 export PYSPARK_PYTHON=/home/ubuntu/shrink_venv/bin/python3
 export PYSPARK_DRIVER_PYTHON=/home/ubuntu/shrink_venv/bin/python3
-export CORES_PER_WORKER=1
+export CORES_PER_WORKER=3
 export TOTAL_CORES=$((${CORES_PER_WORKER}*${SPARK_WORKER_INSTANCES}))
 export AWS_REGION=us-east-1
 
 ${SPARK_HOME}/bin/spark-submit \
 --master ${MASTER} \
---packages databricks:spark-deep-learning:1.2.0-spark2.3-s_2.11 \
 --conf spark.cores.max=${TOTAL_CORES} \
 --conf spark.task.cpus=${CORES_PER_WORKER} \
 --conf spark.task.maxFailures=1 \
 --conf spark.stage.maxConsecutiveAttempts=1 \
 --conf spark.executorEnv.JAVA_HOME="$JAVA_HOME" \
 --conf spark.executorEnv.AWS_REGION=${AWS_REGION} \
-moving_images.py
+--executor-memory 3G \
+producer_distributed.py \
+--src_bucket_name "insight-data-images" \
+--src_prefix "dataset/not_occluded/" \
+--src_type "test"  \
+--des_bucket_name "insight-deep-images-hub" \
+--source_file "/home/ubuntu/sample.txt"
 
